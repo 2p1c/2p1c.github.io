ba26b3fe3eeea7f7aa8bbf3f195c5ee9ad364d8b
akidforseven@gmail.com <akidforseven@gmail.com>
2025-12-13 23:12:24 +0800

    feature: 添加了顶部丝滑导航栏，后续还需要添加子网站；添加了页脚信息，页脚还可以放置其他网站链接信息；如何设置页面长度，将页脚放置在页面的最底部，下一步需要整理元素，完善可扩展框架，排版以及编辑页面内容

--- assets/js/2p1c_js_file.js
+++ assets/js/2p1c_js_file.js
@@ -1,12 +1,13 @@
 const list = document.createElement("ul");
 const info = document.createElement("p");
 const html = document.querySelector("html");
+const main = document.querySelector("main");
 
 info.textContent =
   "Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.";
 
-document.querySelector(".smooth-scroll").appendChild(info);
-document.querySelector('.smooth-scroll').appendChild(list);
+main.appendChild(info);
+main.appendChild(list);
 
 // html.onclick = function() {
 //   const listItem = document.createElement('li');
@@ -20,3 +21,17 @@ document.querySelector('.smooth-scroll').appendChild(list);
 //     this.textContent = listContent;
 //   }
 // }
+// 获取当前年份
+  const year = new Date().getFullYear();
+  // 找到 ID 为 "currentYear" 的 span 元素
+  const yearElement = document.getElementById("currentYear");
+  
+  // 检查起始年份是否小于当前年份。
+  // 如果起始年份是 2025，而当前年份已经是 2026 或更大，
+  // 则显示年份范围，例如 "2025–2026"
+  if (year > 2025) {
+      yearElement.textContent = `2025–${year}`;
+  } else {
+      // 如果当前年份是 2025，则只显示 2025
+      yearElement.textContent = year;
+  }
